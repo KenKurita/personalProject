b2476e05555ba00eda8964a329fed842b5b2e472
Ken Kurita <45981670+KenKurita@users.noreply.github.com>
2022-10-11 19:36:12 -0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.join(__dirname, '/client/src', '/index.jsx'),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.join(__dirname,"/client/public"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  }
+};
